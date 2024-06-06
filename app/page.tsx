1dd3c8c98fc26ae9747a4e9a7d978251a16be915
Muhammad <muhammadfabil40@gmail.com>
2024-06-06 17:40:39 +0700

    next js init

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="relative">
+      <div>
+        <h1 className="text-red-500">
+          hi
+        </h1>
+      </div>
+    </main>
+  );
+} 
